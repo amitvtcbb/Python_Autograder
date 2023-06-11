51a45e94e98435ddb6dbe1c5ab8f9fe485fe5aeb
PROF. AMIT PATEL <76394549+amitvtcbb@users.noreply.github.com>
2023-06-11 10:38:23 +0530

    Update autograder_prog2.py

--- .autograder/autograder_prog2.py
+++ .autograder/autograder_prog2.py
@@ -1,40 +1,37 @@
-import math
-
-def is_prime(number):
-    if number < 2:
+def is_prime(num):
+    # Check if the number is prime
+    if num < 2:
         return False
-    for i in range(2, int(math.sqrt(number)) + 1):
-        if number % i == 0:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
-def autograder():
-    # Read input from input.txt file
-    with open('Input_Output/input2.txt', 'r') as file:
-        number = int(file.read().strip())
+# Function to generate feedback for program 2
+def generate_program2_feedback(expected_output, student_output):
+    if expected_output == student_output:
+        grade = 100
+        feedback = "Congratulations! Your solution for program 2 is correct."
+    else:
+        grade = 0
+        feedback = "Oops! Your solution for program 2 is incorrect. Please check your code."
 
-    # Execute the student's solution script
-    # Assuming the student's solution script is named "student_solution.py"
-    exec(open("student_solution.py").read())
+    feedback += f"\nGrade: {grade}%"
+    return feedback
 
-    # Check if the number is prime using the student's solution
-    student_output = is_prime(number)
+# Read the input number from the input.txt file
+with open('Input_Ouput/input2.txt', 'r') as file:
+    input_number = int(file.readline().strip())
 
-    # Compare the student's output with the expected output from output.txt file
-    with open('Input_Output/output2.txt', 'r') as file:
-        expected_output = bool(file.read().strip())
+# Calculate the expected output for the input number
+expected_output = is_prime(input_number)
 
-    # Compare the student's output with the expected output
-    if student_output == expected_output:
-        score = 100
-    else
-        score = 0
+# Read the student's output from the output.txt file
+with open('Input_Ouput/output2.txt', 'r') as file:
+    student_output = bool(file.readline().strip())
 
-    # Return the score
-    return score
+# Generate feedback for program 2 based on the expected and student's output
+feedback = generate_program2_feedback(expected_output, student_output)
 
-# Call the autograder function
-score = autograder()
-
-# Print the score
-print("Score:", score)
+# Print the feedback
+print(feedback)
