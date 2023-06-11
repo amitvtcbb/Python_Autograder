396c3ce3b8d50468bf2740228f5b0a7731c9a5e0
PROF. AMIT PATEL <76394549+amitvtcbb@users.noreply.github.com>
2023-06-11 10:17:52 +0530

    Create .autograder_prog1.py

---
+++ .autograder/.autograder_prog1.py
@@ -0,0 +1,30 @@
+def autograder():
+    # Read input from input.txt file
+    with open('input_output/input.txt', 'r') as file:
+        number = int(file.read().strip())
+
+    # Execute the student's solution script
+    # Assuming the student's solution script is named "student_solution.py"
+    exec(open("student_solution.py").read())
+
+    # Calculate the sum of digits using the student's solution
+    student_output = calculate_digit_sum(number)
+
+    # Compare the student's output with the expected output from output.txt file
+    with open('input_output/output.txt', 'r') as file:
+        expected_output = int(file.read().strip())
+
+    # Compare the student's output with the expected output
+    if student_output == expected_output:
+        score = 100
+    else:
+        score = 0
+
+    # Return the score
+    return score
+
+# Call the autograder function
+score = autograder()
+
+# Print the score
+print("Score:", score)
